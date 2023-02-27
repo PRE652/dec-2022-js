520a0824428631b8fc59d5c240bcd18e60c013df
652 Name <pakotin777@gmail.com>
2023-02-27 17:48:16 +0200

    dec-2022-js.iml

---
+++ lessons/lesson 2/main1.js
@@ -0,0 +1,270 @@
+let names = ['vasya',
+    'petya',
+    'anna',
+    'olya',
+    'nazar',
+    'pasha',
+    'misha',
+    'natali',
+    'liza',
+    `karina`]
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
+console.log(names[4])
+console.log(names[5])
+console.log(names[6])
+console.log(names[7])
+console.log(names[8])
+console.log(names[9])
+
+
+
+
+
+let book1 = {
+    title: 'book',
+    pageCount: 998,
+    genre: 'horror',
+}
+
+console.log(book1);
+
+
+let book2 = {
+    title: 'great book',
+    pageCount: 4448,
+    genre: 'good book',
+}
+console.log(book2)
+
+let book3 = {
+    title: 'macBook',
+    pageCount: 4343434,
+    genre: 'adult',
+}
+console.log(book3)
+
+
+let book4 = {
+    title: 'book for brain',
+    pageCount: 43434,
+    genre: 'adult',
+    authors: [
+        {
+            name: 'Vasya',
+            age:23
+        },
+        {
+            name: 'Nazar',
+            age:44
+
+        }
+
+    ],
+
+}
+console.log(book4)
+
+
+let book5 = {
+    title: 'food for brain',
+    pageCount: 3343444,
+    genre: 'Master',
+    authors: [
+        {
+            name: 'Anya',
+            age:20
+        },
+        {
+            name: 'Nastya',
+            age:40
+
+        }
+
+    ],
+
+}
+console.log(book5)
+
+let book6 = {
+    title: 'book for children',
+    pageCount: 444,
+    genre: 'cinema',
+    authors: [
+        {
+            name: 'Liza',
+            age:43
+        },
+        {
+            name: 'Misha',
+            age:55
+
+        }
+
+    ],
+
+}
+console.log(book6)
+
+
+let users = [
+    {name: 'Pasha', username: 'Pasha213', password: 1111},
+    {name: 'Sasha', username: 'Sasha213', password: 2222},
+    {name: 'Nadia', username: 'Nadia213', password: 3333},
+    {name: 'Oleg', username: 'Oleg213', password: 4444},
+    {name: 'Nazar', username: 'Nazar213', password: 5555},
+    {name: 'Nastya', username: 'Nastya213', password: 6666},
+    {name: 'Vika', username: 'Vika213', password: 7777},
+    {name: 'Tanya', username: 'Tanya213', password: 8888},
+    {name: 'Ostap', username: 'Ostap213', password: 9999},
+    {name: 'Olena', username: 'Olena213', password: 10000},
+    {name: 'Oksana', username: 'Oksana213', password: 11000},
+    {name: 'Karina', username: 'Karina213', password: 12000},
+
+];
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
+console.log(users[10].password);
+console.log(users[11].password);
+
+let x = 2
+if (x !== 0)
+{
+    console.log('right');
+}
+else {
+    console.log('false');}
+
+
+
+let a = 1
+if (a !== 0)
+{
+    console.log('right');
+}
+else {
+    console.log('false');}
+
+let b = 0
+if (b !== 0)
+{
+    console.log('right');
+}
+else {
+    console.log('false');}
+
+let c = -3
+if (c !== 0)
+{
+    console.log('right');
+}
+else {
+    console.log('false');}
+
+let time = 19
+
+if (time > 0 && time <= 15) {
+        console.log('1');}
+    else if (time>15 && time <= 30) {
+        console.log('2');}
+    else if (time > 30 && time <= 45) {
+        console.log('4');}
+        else if (time > 45 && time <= 60){
+    }
+
+let day = 23
+if (day > 0 && day <= 15) {
+    console.log('1');}
+else if (day>15 && day <= 30) {
+    console.log('2');}
+else if (day > 30 && day <= 45) {
+    console.log('3');
+}
+
+let days = 1;
+switch (days) {
+    case 1:
+        console.log('monday') ;
+        break;
+
+    case 2:
+        console.log('tuesday') ;
+        break;
+
+    case 3:
+        console.log('Wednesday') ;
+        break;
+
+    case 4:
+        console.log('Thursday') ;
+        break;
+
+    case 5:
+        console.log('Friday') ;
+        break;
+
+    case 6:
+        console.log('Sanday') ;
+        break;
+
+    case 7:
+        console.log('Saturday') ;
+        break;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
